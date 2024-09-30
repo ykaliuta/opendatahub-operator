aea4220b7f76b1b91d072795dcf396125fde9481
Yauheni Kaliuta <ykaliuta@redhat.com>
2024-09-30 11:29:35 +0300

    logger: change visible level with zap settings
    With logr it is possible to decrease logging priority of a subsystem
    by passing logger.V(<verbosity>) down the stack, the <verbosity>
    value is then added to the level of the message making it less
    important.
    
    This approach was used in the operator, but it causes some
    inconsitences, like [1], where setting desired level to
    production/error info messages are still shown.
    
    Fix it by changing filtering on the logging backend level.
    
    [1] https://issues.redhat.com/browse/RHOAIENG-12712?focusedId=25642670&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-25642670
    
    Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,18 +2,11 @@ package logger
 
 import (
 	"os"
-	"strings"
 
 	"github.com/go-logr/logr"
 	"go.uber.org/zap/zapcore"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
-
-var logLevelMapping = map[string]int{
-	"devel":   0,
-	"default": 1, // default one when not set log-mode
-	"prod":    2,
-}
 
 // NewNamedLogger creates a new logger for a component.
 // If the mode is set (so can be different from the default one),
@@ -25,16 +18,6 @@ func NewNamedLogger(log logr.Logger, name string, mode string) logr.Logger {
 	return log.WithName(name)
 }
 
-// in each controller, to use different log level.
-func LogWithLevel(logger logr.Logger, level string) logr.Logger {
-	level = strings.TrimSpace(level)
-	verbosityLevel, ok := logLevelMapping[level]
-	if !ok {
-		verbosityLevel = 1 // fallback to info level
-	}
-	return logger.V(verbosityLevel)
-}
-
 // in DSC component, to use different mode for logging, e.g. development, production
 // when not set mode it falls to "default" which is used by startup main.go.
 func NewLogger(mode string) logr.Logger {
@@ -44,14 +27,14 @@ func NewLogger(mode string) logr.Logger {
 		opts = zap.Options{
 			Development:     true,
 			StacktraceLevel: zapcore.WarnLevel,
-			Level:           zapcore.InfoLevel,
+			Level:           zapcore.DebugLevel,
 			DestWriter:      os.Stdout,
 		}
 	case "prod", "production": // the least logging verbosity
 		opts = zap.Options{
 			Development:     false,
 			StacktraceLevel: zapcore.ErrorLevel,
-			Level:           zapcore.InfoLevel,
+			Level:           zapcore.ErrorLevel,
 			DestWriter:      os.Stdout,
 			EncoderConfigOptions: []zap.EncoderConfigOption{func(config *zapcore.EncoderConfig) {
 				config.EncodeTime = zapcore.ISO8601TimeEncoder // human readable not epoch
